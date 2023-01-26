e207e06cafca7bb53bf551085084f428344569ee
Rituraj <rn26rituraj@gmail.com>
2023-01-26 21:38:56 +0530

    adding hero and about

--- pages/components/about.js
+++ pages/components/about.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div className="row">
+        <div class="about-text">
+          <p>As a computer science student, I am eager to apply my 
+          knowledge and skills to real-world projects. I am currently
+          learning and working with the MERN stack, as well as proficient
+          in C, C++, and Java. I am excited to continue honing my skills 
+          and gaining experience in the field.</p><br/>
+          <p>I have a Bachelor's degree in Computer Science from XYZ University
+            and I am currently working as a software engineer at ABC company. 
+            In my free time, I enjoy exploring new technologies and staying 
+            up-to-date with the latest advancements in my field.</p>
+            <br/>
+            <p>If you'd like to get in touch, feel free to connect with me on LinkedIn or email me at example@email.com.
+</p>
+        </div>
+        <div class="about-img">c</div>
+      </div>
+    </>
   )
 }
 
--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -2,7 +2,40 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <>
+      <nav className="navbar">
+        <div className="menu-left">
+
+          <div className="logo">
+            <h2>My Portfolio</h2>
+          </div>
+          <ul className="menu">
+            <li>
+              <a href="" className="menu-link">Home</a>
+            </li>
+            <li>
+              <a href="" className="menu-link">About</a>
+            </li>
+            <li>
+              <a href="" className="menu-link">Work</a>
+            </li>
+            <li>
+              <a href="" className="menu-link"></a>
+            </li>
+            <li>
+              <a href="" className="menu-link"></a>
+            </li>
+          </ul>
+        </div>
+        <div className="menu-right">
+          <ul className="menu">
+            <li>
+              <a href="#" className="menu-link btn">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   )
 }
 
